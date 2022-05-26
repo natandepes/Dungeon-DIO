9e2ef4a4d01de5b2de687bb500c727fe99a38570
Natan Depes <103089417+natandepes@users.noreply.github.com>
2022-05-26 03:16:11 -0300

    Update Program.cs

--- src/Program.cs
+++ src/Program.cs
@@ -12,25 +12,28 @@ namespace RPG{
              
             
             //Console.WriteLine(hero); //toda vez que voce tenta escrever um objeto o console
-            // automaticamente usa o ToString, metodo padrao que eu dei override na classe Hero
+            // automaticamente usa o ToString, metodo padrao que eu dei override na classe abstrata Hero
             
             // Apresentando as características e atacando como Arus(Knight)
             Console.WriteLine(arus);
             Console.WriteLine(arus.Attack());
             Console.WriteLine(arus.Attack(4));
             Console.WriteLine(arus.Attack(7));
+            
             // Apresentando as características e atacando como Jenica(White Wizard)
             Console.WriteLine(); // pulando uma linha para ficar mais legível
             Console.WriteLine(jennica);
             Console.WriteLine(jennica.Attack());
             Console.WriteLine(jennica.Attack(4));
             Console.WriteLine(jennica.Attack(7));
+            
             // Apresentando as características e atacando como Topapa(Black Wizard)
             Console.WriteLine(); // pulando uma linha para ficar mais legível
             Console.WriteLine(topapa);
             Console.WriteLine(topapa.Attack());
             Console.WriteLine(topapa.Attack(4));
             Console.WriteLine(topapa.Attack(7));
+            
             // Apresentando as características e atacando como Wedge(Ninja)
             Console.WriteLine(); // pulando uma linha para ficar mais legível
             Console.WriteLine(wedge);
